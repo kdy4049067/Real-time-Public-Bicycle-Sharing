3d1eda9fec7e570d2ec7cb824c1639b12da8a2bb
daeyeon <kdy4410121@naver.com>
2025-09-05 14:07:24 +0900

    feat: 시큐리티 설정에 필터 추가

--- src/main/java/com/bike/public_bike/auth/config/SecurityConfig.java
+++ src/main/java/com/bike/public_bike/auth/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.bike.public_bike.auth.config;
 
 import com.bike.public_bike.auth.filter.JwtAuthenticationFilter;
+import com.bike.public_bike.auth.token.JwtTokenProvider;
+import com.bike.public_bike.auth.token.TokenExtractor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -29,11 +32,12 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable) // 예전 authorization 인증 방식
                 .formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 방식 disable
                 .logout(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션 사용하지 않으므로 stateless로 설정
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/auth/signup", "/", "/auth/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
