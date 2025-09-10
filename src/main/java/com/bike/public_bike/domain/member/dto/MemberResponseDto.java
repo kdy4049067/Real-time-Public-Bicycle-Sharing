a54cb387be25ec538c21b2892d15d5bbdc78ac55
daeyeon <kdy4410121@naver.com>
2025-09-10 14:33:25 +0900

    feat: 멤버 dto에 id 필드 추가

--- src/main/java/com/bike/public_bike/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/bike/public_bike/domain/member/dto/MemberResponseDto.java
@@ -1,6 +1,7 @@
 package com.bike.public_bike.domain.member.dto;
 
 public record MemberResponseDto(
+        Long id,
         String passwordEncode,
         String nickname,
         Integer point,
